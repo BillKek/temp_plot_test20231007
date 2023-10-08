6e971bae7639dd07db08e2687c3d57f8fada4acd
BillKek <51540758+BillKek@users.noreply.github.com>
2023-10-08 05:31:04 +0300

    Update data1.js

--- data1.js
+++ data1.js
@@ -1,7 +1,11 @@
-var data = [
+{
+const dataadd = [
   {
     x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
     y: [1, 0, 6],
     type: 'scatter'
   }
 ];
+data[0].x = data[0].x.concat(dataadd[0].x);
+data[0].y = data[0].y.concat(dataadd[0].y);
+}
